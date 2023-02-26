88accaca788ebf97e87c7f2fd964f890e1736fd7
Данила Тихонов <danilatihonov@MacBook-Air--Danila.local>
2023-02-26 15:51:43 +0530

    Задача 15

---
+++ work15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+int day = 8;
+int maxPos = 8;
+
+if(maxPos > day){
+    if(day >= 7){
+        Console.WriteLine("Yes");
+    }
+    else{
+        Console.WriteLine("No");
+    }
+}
+else{
+    Console.WriteLine("Error");
+}
