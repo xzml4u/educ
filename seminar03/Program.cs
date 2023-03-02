03b2788578a8682c7494979ea76038d3c069fe35
Данила Тихонов <danilatihonov@MacBook-Air--Danila.local>
2023-03-03 01:05:46 +0530

    Seminar03

---
+++ seminar03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите длину: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0){
+        Console.WriteLine(i);
+    }
