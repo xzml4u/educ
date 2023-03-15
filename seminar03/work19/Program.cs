84fadd6d7c15f6f23694aafcba08894061156521
Данила Тихонов <danilatihonov@MacBook-Air--Danila.local>
2023-03-15 14:58:24 +0530

    add files

--- seminar03/work19/Program.cs
+++ seminar03/work19/Program.cs
@@ -4,15 +4,20 @@
 // 12821 -> да
 // 23432 -> да
 
-void Polindrom (int number){
+using System;
+
+void Polindrom(int number)
+{
     int N1 = number / 10000;
     int N2 = (number / 1000) % 10;
     int N3 = (number % 100) / 10;
     int N4 = number % 10;
-    if (N1 == N4 && N2 == N3){
+    if (N1 == N4 && N2 == N3)
+    {
         Console.WriteLine("Yes");
     }
-    else {
+    else
+    {
         Console.WriteLine("No");
     }
 }
